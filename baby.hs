2c42a137c01b14a6a6824698f6a8ef911e792909
John Allison <john123allison@gmail.com>
2021-09-23 15:38:04 -0400

    fix errors

--- baby.hs
+++ baby.hs
@@ -2,22 +2,25 @@ doubleMe x = x + x
 
 doubleUs x y = x * 2 + y * 2
 
-doubleSmallNumber = if x > 100
+doubleSmallNumber x = if x > 100
                        then x
                        else x * 2
 
-doubleSmallNumber' = (if x > 100 then x else x * 2) + 1
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
 
 lostNumbers = [4,8,15,16,23,42]
 someOthers = [2,5]
 bothLists = lostNumbers ++ someOthers
 
-# Strings are just lists of characters
+-- Strings are just lists of characters
 aSmallCat = 'A':" SMALL CAT"
 
-# Indexing
+-- Indexing
 capitalB = "Steve Buscemi" !! 6
 
-# Texas ranges
+-- Texas ranges
 oneToTwenty = [1..20]
 aToZ = ['a'..'z']
+
+-- List comprehensions
+evensInOneTwenty = [x | x <- oneToTwenty, x `mod` 2 == 0]
